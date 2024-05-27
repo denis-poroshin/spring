240052db41fb222abf555e42152b47bdeadd7b89
original <git@github.com:denis-poroshin/variables.git>
2024-05-27 18:28:44 +0300

    Начало

---
+++ src/main/java/pro/sky/skyprospring/SkyproSpringApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringApplication.class, args);
+	}
+
+}
